ef2cd2df296c80562ad26f3e2fd56e36d0c0d8e1
YoungNovice <2306915539@qq.com>
2019-02-05 10:22:59 +0800

    stack init

---
+++ stack/sstack.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+typedef struct Stack {
+	int cap;
+	int size;
+	int *pBase;
+	int *pTop;
+} STACK, *PSTACK;
+
+void init_stack(PSTACK, cap);
+void traverse_stack(PSTACK);
+bool push(PSTACK, int);
+int pop(PSTACK);
+
+
+int main(void) {
+	STACK s;
+	init_stack(&s, 10);
+	traverse_stack(&s);
+	
+
+	return 0;
+}
+
+void push(PSTACK pStack, int val) {
+	pStack->pBase
+
+}
+
+void traverse_stack(PSTACK pStack) {
+	if(NULL == pStack && 0 == pStack->size) {
+		printf("stack empty");
+		exit(-1);
+	}
+	for(int i=pStack->size-1; i>0; i--) {
+		prinf("%d\t", *(pStack->pBase-i) );
+	}
+	printf("\n");
+}
+
+void init_stack(PSTACK pStack, int cap) {
+	pStack->pBase = (int *)malloc(sizeof(STACK) * cap);
+	if (NULL == pStack->pBase) {
+		printf("init err");
+		exit(-1)
+	} else {
+		pStack->size = 0;
+		pStack->cap = cap;
+	}
+}
