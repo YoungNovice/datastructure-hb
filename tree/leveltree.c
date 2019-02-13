2aeb5f340881cf29e9f0c0f963c058c3b581a446
YoungNovice <2306915539@qq.com>
2019-02-13 16:46:00 +0800

    leveltree

---
+++ tree/leveltree.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+# include <malloc.h>
+/* 层次遍历：通过辅助队列来实现 
+ * 
+ */
+// https://blog.csdn.net/zwx19921215/article/details/83306952
+// 定义一颗树
+typedef struct {
+	char data; // 数据域
+	struct BitTreeNode *lchild, *rchild; // 左孩子 右孩子
+} BitTreeNode, *BitTree;
+
+int main(void) {
+
+	return 0;
+}
